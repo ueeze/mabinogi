85bff33c7cc57144b73e8a317324b97ac990647f
ueeze <janngsujanga@naver.com>
2026-07-05 01:42:45 +0900

    2.2

--- data/abyss.ts
+++ data/abyss.ts
@@ -2,13 +2,16 @@ export const abyssList = [
   {
     id: 'abyss-1',
     title: '허상의 정박지',
+    desc: '',
   },
   {
     id: 'abyss-2',
     title: '광기의 동굴',
+    desc: '',
   },
   {
     id: 'abyss-3',
     title: '흩어진 물길',
+    desc: '',
   },
 ]
--- data/raid.ts
+++ data/raid.ts
@@ -1,5 +1,5 @@
 export const raidList = [
-  { id: 'raid-2', title: '서큐버스' },
-  { id: 'raid-3', title: '타바르타스' },
-  { id: 'raid-4', title: '에이렐' },
+  { id: 'raid-2', title: '서큐버스', desc: '' },
+  { id: 'raid-3', title: '타바르타스', desc: '' },
+  { id: 'raid-4', title: '에이렐', desc: '' },
 ]
